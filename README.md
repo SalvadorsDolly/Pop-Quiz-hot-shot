4618dd4491f02e6aa3205321f2f7027706856325
Marisa Cole <salvadorsdolly@gmail.com>
2017-10-21 14:39:20 -0700

    first commit, early study

---
+++ README.md
@@ -0,0 +1,2 @@
+#Basic javascript code study
+This is an early study for html5 and javascript
---
+++ js/script.js
@@ -0,0 +1,32 @@
+function submitAnswers() {
+	var total = 5;
+	var score = 0; 
+	// varibles for getting answers from the form element quizeForm
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+	//validating answers
+	for(i = 1; i<= total; i++) {
+		if(eval( 'q'+ i ) == null || eval( 'q'+ i ) == ''){
+			alert('You missed answering question ' + i);	
+			return false;
+		}	
+	}
+	// this sets correct answers
+	var answers = ["b", "b", "d", "b", "d"];
+
+	//this will check answers
+	for(i = 1; i<= total; i++){
+		if(eval( 'q'+ i ) == answers[i - 1]) {
+		score ++;
+	   }
+	}
+	// this to display results
+	var results = document.getElementById('results');
+	results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'<span></h3>';
+	alert('You scored '+score+ 'out of ' +total);
+
+	return false;
+}
